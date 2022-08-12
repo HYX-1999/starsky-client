a75c25b6fb6f66895bc0b4f04c9089e5128eeecc
heyuxin <william@rantion.com>
2022-08-12 17:00:14 +0800

    feature: 更新文章列表

--- src/views/home/components/section/index.tsx
+++ src/views/home/components/section/index.tsx
@@ -6,9 +6,31 @@
 
 import React from "react"
 import $style from "./index.scss"
+import PostCard from "./postCard"
 
 const Section = () => {
-  return <section className={$style.section}>section</section>
+  return (
+    <section className={$style.section}>
+      <PostCard
+        title="JavaScript 作用域、作用域链的理解"
+        content={"JavaScript 作用域、作用域链的理解".repeat(100)}
+        time="2021-10-20"
+        tags={["React", "JavaScript", "Git"]}
+      />
+      <PostCard
+        title="JavaScript 作用域、作用域链的理解"
+        content={"JavaScript 作用域、作用域链的理解".repeat(100)}
+        time="2021-10-20"
+        tags={["React", "JavaScript", "Git"]}
+      />
+      <PostCard
+        title="JavaScript 作用域、作用域链的理解"
+        content={"JavaScript 作用域、作用域链的理解".repeat(100)}
+        time="2021-10-20"
+        tags={["React", "JavaScript", "Git"]}
+      />
+    </section>
+  )
 }
 
 export default Section
---
+++ src/views/home/components/section/postCard/index.tsx
@@ -0,0 +1,42 @@
+/*
+ * @Description: PostCard
+ * @Author: hyx
+ * @Date: 2022-08-12 16:09:09
+ */
+
+import Card from "@/components/Card"
+import dayjs from "dayjs"
+import React from "react"
+import $style from "./index.scss"
+
+type Props = {
+  title?: string
+  content?: string
+  time?: string
+  tags?: string[]
+}
+
+const PostCard = ({ title, content, time, tags }: Props) => {
+  return (
+    <Card className={$style.card}>
+      <div className={$style.title}>{title}</div>
+      <p className={$style.content}>
+        {content!
+          .replace(/<a(.*?)>(.*?)<\/a>/g, "$2")
+          .replace(/[# |**|`|>]/g, "")}
+      </p>
+      <div className={$style.info}>
+        <span className={$style.date}>{dayjs(time!).format("YYYY-MM-DD")}</span>
+        <div className={$style.tags}>
+          {tags!.map((tag) => (
+            <span className={$style.tag} key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+export default PostCard
